069a0313d8821c4e4e6e70e4b96951610bb56db8
Stefan <Stefan@adly.com>
2013-03-15 19:14:39 -0700

    added some new models

--- politiburo/models.py
+++ politiburo/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Site(models.Model):
+    score = models.IntegerField(default=0)
+    user_score = models.IntegerField(default=0)
+
+class Author(models.Model):
+    score = models.IntegerField(default=0)
+
+class Article(models.Model):
+    site = models.ForeignKey(Site)
+    author = models.ForeignKey(Author)
+    score = models.IntegerField(default=0)
+    content = models.TextField()
+
+class User(models.Model):
+    name = models.CharField(max_length=10)
+
+class Review(models.Model):
+    article = models.ForeignKey(Article)
+    user = models.ForeignKey(User)
+    comment = models.TextField()
+    score = models.IntegerField(default=0)
