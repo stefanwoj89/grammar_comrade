439283c128ef54cdc2acfdf96be695d4c212d3ed
coreyrecvlohe <corey.recvlohe@gmail.com>
2013-03-16 14:40:26 -0700

    HTML parser test;

--- politiburo/views.py
+++ politiburo/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render_to_response
+from BeautifulSoup import BeautifulSoup
+import urllib2
 
 def index(request):
-	data = []
+	url = 'http://www.economist.com/news/united-states/21573165-years-republican-candidates-sound-awful-lot-last-years-same-again-please?fsrc=rss|ust'
+	soup = BeautifulSoup(urllib2.urlopen(url).read())
 	return render_to_response('home/index.html', {})
 
