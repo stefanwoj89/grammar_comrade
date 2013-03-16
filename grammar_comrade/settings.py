82663b0d2638260dc13d607375cc8fb7310379d9
coreyrecvlohe <corey.recvlohe@gmail.com>
2013-03-16 14:21:20 -0700

    Main template components;

--- grammar_comrade/settings.py
+++ grammar_comrade/settings.py
@@ -105,7 +105,7 @@ ROOT_URLCONF = 'grammar_comrade.urls'
 WSGI_APPLICATION = 'grammar_comrade.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    "/home/ubuntu/politiburo/templates" 
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
--- politiburo/views.py
+++ politiburo/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render_to_response
 
 def index(request):
 	data = []
-    return render_to_response('home/index.html', {})
+	return render_to_response('home/index.html', {})
 
