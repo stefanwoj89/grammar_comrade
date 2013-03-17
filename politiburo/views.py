8f842f0f6308dfc42252897bad837e90c49807f7
coreyrecvlohe <corey.recvlohe@gmail.com>
2013-03-17 13:15:26 -0700

    Create Site, Author, Article if none exist; Find article 2, print content;

--- politiburo/views.py
+++ politiburo/views.py
@@ -10,10 +10,21 @@ def parse_string(el):
    text = ''.join(el.findAll(text=True))
    return text.strip()
 
-def createArticle(string):
-    print string
-    article = Article.objects.create(content=string)
-    article.save()
+def processArticle(string):
+    try:
+        site = Site.objects.get(id=1)
+        try:
+            author = Author.objects.get(id=1)
+            article = Article.objects.create(content=string, site=site, author=author)
+            article.save()
+            print article
+        except Author.DoesNotExist:
+            author = Author.objects.create()
+            author.save()
+    except Site.DoesNotExist:
+        site = Site.objects.create()
+        site.save()
+
 
 def process_html(text):
 	string = ''
@@ -25,16 +36,22 @@ def process_html(text):
             #print data
 			if new_row.index(i) != 0:
 				string+=str(i)
-	createArticle(string)
+	processArticle(string)
 
+def createNewArticle():
+    url = 'http://www.economist.com/news/united-states/21573165-years-republican-candidates-sound-awful-lot-last-years-same-again-please?fsrc=rss|ust'
+    soup = BeautifulSoup(urllib2.urlopen(url).read())
+    rows = soup('article')
+    results = process_html(rows)
+
+def findArticle():
+    article = Article.objects.get(id=2)
+    print article.content
 
 def index(request):
-	url = 'http://www.economist.com/news/united-states/21573165-years-republican-candidates-sound-awful-lot-last-years-same-again-please?fsrc=rss|ust'
-	soup = BeautifulSoup(urllib2.urlopen(url).read())
-	rows = soup('article')
-	results = process_html(rows)
-
-	return render_to_response('home/index.html', {})
+    #createNewArticle()
+    findArticle()
+    return render_to_response('home/index.html', {})
 
 def generate_article_score(content):
     ATD.setDefaultKey(settings.ATD_API_KEY)
